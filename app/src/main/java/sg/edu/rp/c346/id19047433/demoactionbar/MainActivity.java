408946e3b2f60dacc2a118ee4a832324467fc217
19047433 <19047433@myrp.edu.sg>
2021-05-27 16:07:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19047433/demoactionbar/MainActivity.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.id19047433.demoactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
